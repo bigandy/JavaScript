6482602eb6cebfb069ab9971d25f10beca17d656
Andrew Hudson <ahudson@gmail.com>
2014-02-05 14:43:59 +0000

    get canvas working, woop

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -1,28 +1,34 @@
-/* global canvas */
-(function ($, window, undefined) {
-    'use strict';
-	var AH = {};
+/* global canvas, AH */
+'use strict';
+window.AH = {};
 
-	AH.drawCanvas = function() {
-		// var self = this;
+AH.drawCanvas = function() {
+	this.canvas = document.getElementById('canvas');
+	this.ctx = canvas.getContext('2d');
+	this.width = canvas.width;
+	this.height = canvas.height;
+	this.drawLines(this.ctx);
+};
 
-		this.canvas = document.getElementById('canvas');
-		this.ctx = canvas.getContext('2d');
-		this.width = canvas.width;
-		this.height = canvas.height;
-		this.drawlines(this.ctx);
+AH.drawLines = function(ctx) {
 
-	};
+	ctx.strokeStyle = 'green';
+    ctx.lineWidth = '10';
+    ctx.fillStyle = 'orange';
+	ctx.beginPath();
+	ctx.moveTo(0,this.height / 2);
+	ctx.lineTo(this.width/2, this.height/2);
+    ctx.lineTo(0, this.height - ctx.lineWidth/2);
+    ctx.lineTo(this.width, this.height - ctx.lineWidth/2);
+    ctx.lineTo(this.width / 2 - ctx.lineWidth / 2, 0);
+    ctx.lineTo(0,this.height / 2);
+	ctx.stroke();
+    ctx.fill();
+};
 
-	AH.drawLines = function(ctx) {
-		ctx.strokeStyle = '#ff0000';
-		ctx.beginPath();
-		ctx.moveTo(0,0);
-		ctx.lineTo(this.width/2, this.height/2);
-		ctx.stroke();
-	};
+AH.drawCanvas();
 
 
 
-})(jQuery, this);
+
 
