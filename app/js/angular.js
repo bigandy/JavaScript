3cf623693428dcfba4034c40be8b0b564e79e626
Andrew Hudson <ahudson@gmail.com>
2014-02-13 17:06:53 +0000

    make canvas more object orientated

--- app/js/angular.js
+++ app/js/angular.js
@@ -1,14 +1,14 @@
 'use strict';
 
-var Ctrl = function ($scope) {
-	$scope.name = 'Andrew JD Hudson';
-};
+// var Ctrl = function ($scope) {
+// 	$scope.name = 'Andrew JD Hudson';
+// };
 
-var AnotherCtrl = function ($scope) {
-	$scope.age = '33';
+// var AnotherCtrl = function ($scope) {
+// 	$scope.age = '33';
 
 
-	$scope.$watch('name', function () {
-		console.log($scope.name);
-	});
-};
+// 	$scope.$watch('name', function () {
+// 		console.log($scope.name);
+// 	});
+// };
--- app/js/canvas.js
+++ app/js/canvas.js
@@ -2,31 +2,103 @@
 'use strict';
 window.AH = {};
 
-AH.drawCanvas = function() {
+AH.init = function() {
 	this.canvas = document.getElementById('canvas');
 	this.ctx = canvas.getContext('2d');
 	this.width = canvas.width;
 	this.height = canvas.height;
-	this.drawLines(this.ctx);
+
+    this.drawGrid(this.ctx);
+    // Call other functions
+    var list = [
+        0, 100, 200, 300, 400, 500, 600
+    ];
+
+
+    var self = this;
+    this.boxList = [];
+
+    for (var i = list.length; i--;) {
+        var box = new AH.Box(self.ctx, list[i]+10, list[i]+10, 90, self.height - list[i] - 20);
+        box.render(); // output the box
+        self.boxList.push(box); // put the dimensions of the boxes in an array for later use
+    }
+
+
 };
 
-AH.drawLines = function(ctx) {
+AH.drawGrid = function(ctx) {
 
-	ctx.strokeStyle = 'green';
-    ctx.lineWidth = '10';
-    ctx.fillStyle = 'orange';
-	ctx.beginPath();
-	ctx.moveTo(0,this.height / 2);
-	ctx.lineTo(this.width/2, this.height/2);
-    ctx.lineTo(0, this.height - ctx.lineWidth/2);
-    ctx.lineTo(this.width, this.height - ctx.lineWidth/2);
-    ctx.lineTo(this.width / 2 - ctx.lineWidth / 2, 0);
-    ctx.lineTo(0,this.height / 2);
-	ctx.stroke();
+    ctx.shadowBlur = 0;
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0;
+
+    ctx.strokeStyle = 'black';
+    ctx.lineWidth = '1';
+    ctx.lineJoin = 'round';
+    ctx.beginPath();
+    ctx.moveTo(10,10);
+    ctx.lineTo(10,this.height-10);
+    ctx.lineTo(this.width-5,this.height-10);
+    ctx.stroke();
+
+
+    var topBottomMarker = this.height - 15,
+        bottomBottomMarker = this.height - 5,
+        markers = [
+            100, 200, 300, 400, 500, 600
+        ];
+
+
+    for (var i = markers.length; i--;) {
+        ctx.moveTo(markers[i],topBottomMarker);
+        ctx.lineTo(markers[i],bottomBottomMarker);
+        ctx.stroke();
+
+        ctx.moveTo(5,markers[i]);
+        ctx.lineTo(15,markers[i]);
+        ctx.stroke();
+    }
+
+    ctx.save();
+};
+
+
+
+
+AH.Box = function(ctx, x, y, height, width) {
+    this.ctx = ctx;
+    this.x = x;
+    this.y = y;
+    this.height = height;
+    this.width = width;
+};
+
+
+AH.Box.prototype.render = function () {
+    this.ctx.fillStyle = 'orange';
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, this.height, this.width);
+    this.ctx.closePath();
+    this.ctx.fill();
+};
+
+AH.drawCircle = function(ctx) {
+
+    ctx.beginPath();
+    // ctx.moveTo(100,10);
+
+    ctx.arc(this.width - 200, 200, 100, 0, 2*Math.PI, true); // x,y,radius,angleStart, angleEnd, anticlockwise
+    ctx.shadowColor = '#999';
+    ctx.shadowBlur = 20;
+    ctx.shadowOffsetX = -15;
+    ctx.shadowOffsetY = 0;
+
+    // ctx.stroke();
     ctx.fill();
 };
 
-AH.drawCanvas();
+AH.init();
 
 
 
