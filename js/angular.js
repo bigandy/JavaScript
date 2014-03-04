72d2f4deb9803c16b86cdaf42bcc0042af63ef65
Andrew Hudson <ahudson@gmail.com>
2014-03-04 13:14:52 +0000

    continue work in angular tut

--- js/angular.js
+++ js/angular.js
@@ -1,2 +1,15 @@
+/* global console */
 'use strict';
 
+var Ctrl = function ($scope) {
+    $scope.name = 'Andrew Hudson';
+    $scope.age = '33';
+};
+
+var AnotherCtrl = function ($scope) {
+    $scope.name = 'Andrew Hudson';
+    $scope.age = '33';
+    $scope.$watch('name', function(){
+        console.log($scope.name);
+    });
+};
