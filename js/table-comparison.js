c9b5a2367b938c7c663431b24e478d472702cbde
Andrew Hudson <ahudson@gmail.com>
2014-12-11 17:01:52 +0000

    update table code

--- js/table-comparison.js
+++ js/table-comparison.js
@@ -15,43 +15,82 @@ AH.cellClick = function () {
 			parent = $this.parent('tr'),
 			years,
 			standard,
-			level;
+			level,
+			yearsTrue,
+			outputText = '';
 
+		// if you click on the element with parent that doesn't have class tr--active,
+		// remove text from all .sub-output elements
+		if (true === AH.checkRow(parent)) {
+			console.log('false');
+			$('.sub-output').text('');
+
+
+		} else {
+			// highlight the row
+			AH.highlightRow(parent);
+		}
+		// add class to the clicked td
 		$this.addClass('td--active');
 		// remove class from other of same class
 		$this.siblings('td.' + tdClass).removeClass('td--active');
-
-
 
 		if ($this.data()) {
 			parent.data($this.data());
 
 			parent.data(tdClass, $this.data(tdClass));
-			$('h1.' + tdClass).text(parent.data(tdClass));
+			$('div.' + tdClass).text(parent.data(tdClass));
 		}
 
+		// checks to see if there is a parent.data('level'),
+		// assign if is, otherwise let it equal 1
+		// TODO : way of looping these so less code.
 
-		if (typeof(parent.data('level')) !== 'undefined') {
+		if ('undefined' !== typeof(parent.data('level'))) {
 			level = parent.data('level');
 		} else {
 			level = 1;
 		}
 
-		if (typeof(parent.data('standard')) !== 'undefined') {
+		if ('undefined' !== typeof(parent.data('standard'))) {
 			standard = parent.data('standard');
 		} else {
 			standard = 1;
 		}
 
-		if (typeof(parent.data('years')) !== 'undefined') {
+		if ('undefined' !== typeof(parent.data('years'))) {
 			years = parent.data('years');
 		} else {
 			years = 1;
 		}
 
-		$('.output').text(level * standard * years);
+		console.log(level);
 
-		AH.highlightRow(parent);
+		if (1 !== level) {
+			outputText = '<span>Select a Standard and number of Years</span>';
+		} else {
+			outputText = '<span class="negative">Level Has value</span>';
+		}
+
+		if (1 !== standard) {
+			outputText += '<span>Select a Level and number of Years</span>';
+		} else {
+			outputText += '<span class="negative">Standard Has value</span>';
+		}
+
+		if (1 !== years) {
+			outputText += '<span>Select a Level and Standard</span>';
+		} else {
+			outputText += '<span class="negative">Years Has value</span>';
+		}
+
+		console.log(outputText);
+
+		// asign the calculation to the text of .output
+		$('.output').text(level * standard * years);
+		// $('.output-text').html(outputText);
+
+
 	});
 };
 
