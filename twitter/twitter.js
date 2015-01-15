90d9d443ab3a924af0d77dd60ddc5954b819f33e
Andrew Hudson <ahudson@gmail.com>
2015-01-15 11:23:31 +0000

    send tweets from the command line

---
+++ twitter/twitter.js
@@ -0,0 +1,46 @@
+(function () {
+  "use strict";
+
+  function lessThanTenAddZero (value) {
+  	if (value <= 10) {
+  		return "0" + value;
+  	} else {
+  		return value;
+  	}
+  }
+
+  var
+  	config = require('./config.twitter.json'),
+	Twitter = require('node-twitter'),
+	twitter,
+	token,
+	tokenSecret,
+	d = new Date(),
+	tweetContent = "Tweeted at " + d.getHours() + ":" + lessThanTenAddZero(d.getMinutes()) + ":" + lessThanTenAddZero(d.getSeconds()) + " on " + d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear() + " #andrewhudsondev";
+
+
+
+
+	// You could use node-passport and passport-twitter to get an access token easily
+	// See http://blog.coolaj86.com/articles/how-to-tweet-from-nodejs.html
+	twitter = new Twitter.RestClient(
+		config.consumerKey,
+		config.consumerSecret,
+		config.token,
+		config.tokenSecret
+	);
+
+	// Note that you can only direct message someone who follows you
+	twitter.statusesUpdate({
+			status: tweetContent
+			//, in_reply_to_status_id: 357237590082072576
+		},
+		function (err, data) {
+			if (err) {
+				console.error(err);
+			} else {
+				console.log('success! : ' + tweetContent);
+			}
+		}
+	);
+}());
