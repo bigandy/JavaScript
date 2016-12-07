79d7f65bebc1b69e61972c558321050f118c11bf
Andrew Hudson <ahudson@gmail.com>
2016-12-07 22:03:46 +0000

    add wpapi

--- strava/strava.js
+++ strava/strava.js
@@ -1,13 +1,16 @@
 var strava = require('strava-v3');
-
+var config = require('./config.json');
 
 strava.athlete.get({
-    'access_token': '',
-},function(err,payload) {
+    'access_token': config.access_token,
+},function(err, payload) {
     if(!err) {
-        console.log('payload', payload);
+        // console.log('payload', payload);
+		console.log(payload.id);
     }
     else {
         console.log('error:', err);
     }
+
+	return payload.id;
 });
